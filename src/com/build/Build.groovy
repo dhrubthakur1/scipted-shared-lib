5f65e631657effefd91d21de935072884bdb76b3
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 15:30:31 +0530

    Update Build.groovy

--- src/com/build/Build.groovy
+++ src/com/build/Build.groovy
@@ -8,17 +8,14 @@ public class Build {
     }
 
   void execute(def conf = [:], String name="Build") {
+    def maven = script.tool 'MAVEN_PATH'
         script.stage(name) {
           script.withEnv(["PATH+MVN=${script.tool 'MAVEN_PATH'}/bin"]){
            script.bat '''
                 echo "PATH = ${PATH}"
             '''         
         }
-          script.bat "echo ${JAVA_HOME}"
-          script.bat "echo ${MAVEN_HOME}"
-          script.bat "echo ${MAVEN_PATH}"
-          script.bat "echo ${PATH}"
-            script.bat "mvn --version"
+            script.bat "maven --version"
             script.bat "java -version"
             script.bat "echo 'Execute your desired bash command here'"
             script.git url:conf.url, branch:conf.branch
