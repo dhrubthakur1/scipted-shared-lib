e0c9fe53665e11cc0d3a91f202a26f670115c78c
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 18:22:53 +0530

    Update Build.groovy

--- src/com/build/Build.groovy
+++ src/com/build/Build.groovy
@@ -2,33 +2,19 @@ package com.build;
 
 public class Build {
   private final Script script
-  //def maven;
-
+ 
     Build(Script script) {
         this.script = script
-      //this.maven = script.tool 'MAVEN_PATH'
     }
 
   public void execute(def conf = [:], String name="Build") {
     def maven = script.tool 'MAVEN_PATH'
-    def jdk = script.tool 'JAVA_HOME'
+    //def jdk = script.tool 'JAVA_HOME'
         script.stage(name) {
-          script.withEnv(["PATH+MVN=${script.tool 'MAVEN_PATH'}/bin"]){
-           script.bat '''
-                echo "PATH = ${PATH}"
-            '''         
-        }
+          
          script.env.JAVA_HOME="${script.tool 'JAVA_HOME'}"
          script.env.PATH="${script.env.JAVA_HOME}/bin:${script.env.PATH}"
-
-          script.bat "${maven}/bin/mvn --version"
-          script.bat "${jdk}/bin/java -version"
-          
-          script.bat "echo %PATH%"
-         /* script.withEnv(["JAVA_HOME=${jdk}/bin"]) {
-                    //script.bat "${JAVA_HOME}/java -version"
-                    script.bat "${maven}/bin/mvn clean package  -DskipTests"
-                }*/
+              
           script.bat "${maven}/bin/mvn clean package  -DskipTests"
           archive();
         }
