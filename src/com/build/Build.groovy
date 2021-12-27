7e37c324b6876cd2817a77d1614e54ca1dde1f4d
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 17:19:12 +0530

    Update Build.groovy

--- src/com/build/Build.groovy
+++ src/com/build/Build.groovy
@@ -18,6 +18,7 @@ public class Build {
                 echo "PATH = ${PATH}"
             '''         
         }
+          script.bat "set PATH=%PATH%;${jdk}/bin"
           script.bat "${maven}/bin/mvn --version"
           script.bat "${jdk}/bin/java -version"
           script.bat "java -version"
