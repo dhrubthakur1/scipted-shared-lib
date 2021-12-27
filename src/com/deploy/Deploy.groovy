1f9683651b593a73032bffd77136c74b31aa6f2a
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 18:16:01 +0530

    Update Deploy.groovy

--- src/com/deploy/Deploy.groovy
+++ src/com/deploy/Deploy.groovy
@@ -8,7 +8,7 @@ public class Deploy {
     }
    public void execute(def conf=[:], String name='Deploy'){
        script.stage(name) {
-         script.deploy adapters: [steps.tomcat8(credentialsId: conf.tomcatId, path: '', url: conf.tomcatUrl)], contextPath: conf.contextPath, onFailure: false, war: 'target/*.war'
+         script.deploy adapters: [script.tomcat8(credentialsId: conf.tomcatId, path: '', url: conf.tomcatUrl)], contextPath: conf.contextPath, onFailure: false, war: 'target/*.war'
        }
    }
 }
