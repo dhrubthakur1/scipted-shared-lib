9b6e088a7b1ff1ca79b5ba249594035f4bc1d8fe
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 16:10:42 +0530

    Update scriptedSL.groovy

--- vars/scriptedSL.groovy
+++ vars/scriptedSL.groovy
@@ -5,7 +5,7 @@ def call(def conf=[:]) {
    node {
     new CheckOut(this).execute( conf,'CheckOut')
     new ReadSpecFile(this).execute(conf,'Read Spec')
-    stage.bat "echo ${conf}"
+    bat "echo ${conf}"
     new com.build.Build(this).execute(conf,'Build')
 }
 }
