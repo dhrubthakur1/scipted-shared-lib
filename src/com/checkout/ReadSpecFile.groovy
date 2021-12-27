70504528f50e2f8278ae751ac1638b29b6e9da8b
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 14:11:47 +0530

    Update ReadSpecFile.groovy

--- src/com/checkout/ReadSpecFile.groovy
+++ src/com/checkout/ReadSpecFile.groovy
@@ -13,10 +13,19 @@ public class ReadSpecFile{
     script.stage(name) {
      script.bat "dir"
      //script.groovysh.bat "def buildData = readYaml file: 'build.yml' "
-    def buildData = script.readYaml(file: 'build.yml' )
-     script.echo "${buildData}"
-    }
-   
-    //def buildData = readYaml file: 'C:\\Users\\66480\\.jenkins\\workspace\\scripted-shared-lib\\build.yml' 
+     def buildData = script.readYaml(file: 'build.yml' )
+     def deployData = script.readYaml (file: 'deploy.yml') 
+		   
+		   script.echo " ${buildData}"	
+		   script.echo " ${deployData}"
+	    
+		   conf.put('isBuildRequired', buildData.application.buildRequired);
+		   conf.put('buildType', buildData.application.buildType);
+		   conf.put('deployRequired', deployData.application.deployRequired);
+	    conf.put('tomcatId', deployData.application.tomcatId);
+		   conf.put('tomcatUrl', deployData.application.tomcatUrl);
+		   conf.put('contextPath', deployData.application.contextPath);	 		   
+		   script.echo "conf: ${conf}"   
+    }       
 }
 }
