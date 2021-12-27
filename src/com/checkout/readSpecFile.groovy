ab760f97a68a39ae87b1104ca0f29ab3b099140e
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 11:36:32 +0530

    Create readSpecFile.groovy

---
+++ src/com/checkout/readSpecFile.groovy
@@ -0,0 +1,36 @@
+package com.checkout;
+
+puvlic class readSpecFile{
+
+ private final Script script
+
+    readSpecFile(Script script) {
+        this.script = script
+    }
+
+  void execute(def conf = [:], String name="Read Specfile") {
+    script.stage(name) {
+      script{
+        script{                                      
+                   bat "echo ${conf.url}"
+		   checkOut.startBuild(conf)
+		   bat 'echo "read yml start"'
+		   def buildData = readYaml (file: 'build.yml') 
+		   def deployData = readYaml (file: 'deploy.yml') 
+		   bat 'echo "read yml start"'
+		   bat "echo ${buildData}"	
+		   bat "echo ${deployData}"
+	           env.buildRequired=buildData.application.buildRequired
+		   conf.put('isBuildRequired', buildData.application.buildRequired);
+		   conf.put('buildType', buildData.application.buildType);
+		   conf.put('deployRequired', deployData.application.deployRequired);
+	           conf.put('tomcatId', deployData.application.tomcatId);
+		   conf.put('tomcatUrl', deployData.application.tomcatUrl);
+		   conf.put('contextPath', deployData.application.contextPath);	 		   
+		   bat "echo ${env.buildRequired}"
+		   bat "echo conf: ${conf}" 
+                 }
+      }
+        }
+    }
+}
