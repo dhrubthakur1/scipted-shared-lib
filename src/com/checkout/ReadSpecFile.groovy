c551e354881e6b03d501d5d580dd788c915455e3
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 13:07:42 +0530

    Update ReadSpecFile.groovy

--- src/com/checkout/ReadSpecFile.groovy
+++ src/com/checkout/ReadSpecFile.groovy
@@ -12,7 +12,7 @@ public class ReadSpecFile{
 
     script.stage(name) {
      script.bat "dir"
-     //def buildData = readYaml file: 'build.yml' 
+     script "def buildData = readYaml file: 'build.yml' "
     
     }
     def buildData = readYaml file: 'C:\\Users\\66480\\.jenkins\\workspace\\scripted-shared-lib\\build.yml' 
