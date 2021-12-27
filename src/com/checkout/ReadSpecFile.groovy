7ebb53da4243396b4cc34facc1bd2a6dd214fd36
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 12:07:31 +0530

    Update ReadSpecFile.groovy

--- src/com/checkout/ReadSpecFile.groovy
+++ src/com/checkout/ReadSpecFile.groovy
@@ -10,7 +10,14 @@ public class ReadSpecFile{
 
   void execute(def conf = [:], String name="Read Specfile") {
     script.stage(name) {
-     script{bat "echo Triggering ${name} stage..."}
+     script.bat "echo ${conf.url}"		   
+		   script.bat 'echo "read yml start"'
+		   script.bat "def buildData = readYaml (file: 'build.yml') "
+		   script.bat "def deployData = readYaml (file: 'deploy.yml') "
+		   script.bat 'echo "read yml start"'
+		   script.bat "echo ${buildData}"	
+		   script.bat "echo ${deployData}"	          
+		   script.bat "echo conf: ${conf}" 
         }
     }
 }
