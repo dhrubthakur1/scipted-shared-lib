00704590e8a7a682d733197777b96d9faff1fa18
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 16:17:00 +0530

    Update scriptedSL.groovy

--- vars/scriptedSL.groovy
+++ vars/scriptedSL.groovy
@@ -5,7 +5,7 @@ def call(def conf=[:]) {
    node {
     new CheckOut(this).execute( conf,'CheckOut')
     new ReadSpecFile(this).execute(conf,'Read Spec')    
-    if(conf.isBuildRequired == 'No' && conf.buildType == 'Java'){
+    if(conf.isBuildRequired == 'Yes' && conf.buildType == 'Java'){
       new com.build.Build(this).execute(conf,'Build')
     }
   }
