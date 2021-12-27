5d6f46c03840a31d3f75a62c6dc9f1d5538d4b74
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 11:15:35 +0530

    Update CheckOut.groovy

--- src/com/checkout/CheckOut.groovy
+++ src/com/checkout/CheckOut.groovy
@@ -9,9 +9,9 @@ public class CheckOut {
 
   void execute(def conf = [:], String name="Checkout") {
         script.stage(name) {
-            script.bat "Triggering ${name} stage..."
+            script.bat "echo Triggering ${name} stage..."
             script.bat "echo 'Execute your desired bash command here'"
-            script.git url:conf.url, branch:conf.branch
+            
         }
     }
 }
