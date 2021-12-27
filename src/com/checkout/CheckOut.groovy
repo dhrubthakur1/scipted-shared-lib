c43680c8edcd424a5672beaa31c59b790ffebcf0
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 11:02:37 +0530

    Update and rename checkout.groovy to CheckOut.groovy

---
+++ src/com/checkout/CheckOut.groovy
@@ -0,0 +1,17 @@
+package com.checkout
+
+public class CheckOut {
+  private final Script script
+
+    CheckOut(Script script) {
+        this.script = script
+    }
+
+  void execute(def conf = [:], String name="Checkout") {
+        script.stage(name) {
+            script.echo "Triggering ${name} stage..."
+            script.bat "echo 'Execute your desired bash command here'"
+            script.git url:conf.url, branch:conf.branch
+        }
+    }
+}
