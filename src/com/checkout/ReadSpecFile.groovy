9b37f1317080f9cd7f73690c1cd955418448af75
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-27 12:04:00 +0530

    Update ReadSpecFile.groovy

--- src/com/checkout/ReadSpecFile.groovy
+++ src/com/checkout/ReadSpecFile.groovy
@@ -10,25 +10,7 @@ public class ReadSpecFile{
 
   void execute(def conf = [:], String name="Read Specfile") {
     script.stage(name) {
-      script{                                            
-                   bat "echo ${conf.url}"
-		   checkOut.startBuild(conf)
-		   bat 'echo "read yml start"'
-		   def buildData = readYaml (file: 'build.yml') 
-		   def deployData = readYaml (file: 'deploy.yml') 
-		   bat 'echo "read yml start"'
-		   bat "echo ${buildData}"	
-		   bat "echo ${deployData}"
-	           env.buildRequired=buildData.application.buildRequired
-		   conf.put('isBuildRequired', buildData.application.buildRequired);
-		   conf.put('buildType', buildData.application.buildType);
-		   conf.put('deployRequired', deployData.application.deployRequired);
-	           conf.put('tomcatId', deployData.application.tomcatId);
-		   conf.put('tomcatUrl', deployData.application.tomcatUrl);
-		   conf.put('contextPath', deployData.application.contextPath);	 		   
-		   bat "echo ${env.buildRequired}"
-		   bat "echo conf: ${conf}" 
-            }
+      script.bat "echo Triggering ${name} stage..."
         }
     }
 }
