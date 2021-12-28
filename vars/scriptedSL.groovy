1565593a76304f9f73dc38b7d6cb6ad510813c1c
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-28 14:51:45 +0530

    Update scriptedSL.groovy

--- vars/scriptedSL.groovy
+++ vars/scriptedSL.groovy
@@ -4,7 +4,8 @@ import com.deploy.*;
 
 def call(def conf=[:]) {
    node {
-    new CheckOut(this).execute( conf,'CheckOut')
+    //new CheckOut(this).execute( conf,'CheckOut')
+      new CheckOut().execute( conf,'CheckOut')
     new ReadSpecFile(this).execute(conf,'Read Spec')  
      cleanWs()
   }
